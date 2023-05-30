c1d07fdaee2db98d43172e9c913c517c47f17c76
Miron Pawlak <mironpawlak@gmail.com>
2023-05-30 20:45:10 +0200

    Working register + navbar

--- django-app/apps/accounts/views.py
+++ django-app/apps/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse, reverse_lazy
 
-# Create your views here.
+from .forms import RegisterForm
+from django.views.generic import FormView
+
+
+class RegisterView(FormView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
